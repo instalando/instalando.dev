8d9fbaf6941bfda8d39bb3dd0d00cf3cb2d7840e
instalando <instalando@protonmail.com>
2022-04-18 07:41:17 -0300

    Added Icons and social networks to contact

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="grid grid-cols-1 md:grid-cols-12">
     <div class="md:hidden lg:hidden">
       <div class="relative overflow-auto rounded-xl p-8">
-        <div class="dark:highlight-white/5 relative mx-auto flex max-w-sm items-center gap-6 overflow-visible rounded-xl bg-white shadow-lg ring-1 ring-black/5 dark:bg-slate-800">
+        <div
+          class="dark:highlight-white/5 relative mx-auto flex max-w-sm items-center gap-6 overflow-visible rounded-xl bg-white shadow-lg ring-1 ring-black/5 dark:bg-slate-800"
+        >
           <img
             class="absolute -left-6 h-24 w-24 rounded-full shadow-lg"
             src="https://user-images.githubusercontent.com/50197635/163778591-c4e40693-a932-420b-9967-1b285cc8c42e.jpeg"
@@ -10,30 +12,77 @@
           <div class="flex flex-col py-5 pl-24">
             <strong class="text-sm font-medium text-slate-900 dark:text-slate-200">Vitor Soares</strong>
             <span class="text-sm font-medium text-slate-500 dark:text-slate-400">Full-Stack Developer</span>
+            <span class="mt-2">
+              <div>
+                <a
+                  href="https://github.com/instalando"
+                  target="_blank"
+                >
+                  <i class="fa-brands fa-github text-2xl mr-3 text-slate-500 hover:text-white" />
+                </a>
+
+                <a
+                  href="https://t.me/Instalando"
+                  target="_blank"
+                >
+                  <i class="fa-brands fa-telegram text-2xl text-slate-500 hover:text-white mr-3" />
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/instalando/"
+                  target="_blank"
+                >
+                  <i class="fa-brands fa-linkedin text-2xl text-slate-500  hover:text-white" />
+                </a>
+              </div>
+            </span>
           </div>
         </div>
       </div>
       <div class="pointer-events-none absolute inset-0 rounded-xl border border-black/5 dark:border-white/5" />
     </div>
-    
+
     <div class="hidden md:block md:col-span-6 lg:col-span-4">
       <div class="mx-4 my-2 max-w-md overflow-hidden rounded-xl bg-white shadow-md">
-        <div
-          class="md:shrink-0"
-        >
+        <div class="md:shrink-0">
           <img
             class="object-cover w-full aspect-[5/4]"
             src="https://user-images.githubusercontent.com/50197635/163778591-c4e40693-a932-420b-9967-1b285cc8c42e.jpeg"
           >
           <div class="p-8">
-            <div
-              class="text-2xl font-bold text-gray-900 tracking-wide"
-            >
+            <div class="text-2xl font-bold text-gray-900 tracking-wide">
               Vitor Soares <span class="text-base text-slate-500">(Instalando)</span>
             </div>
 
             <div class="mt-2 text-lg tracking-wide text-slate-500">
               Full-Stack Developer
+            </div>
+
+            <div class="mt-2">
+              <div class="inline-block bg-gray-800 p-2 w-full rounded-lg">
+                <div class="ml-2">
+                  <a
+                    href="https://github.com/instalando"
+                    target="_blank"
+                  >
+                    <i class="fa-brands fa-github text-2xl text-gray-400 hover:text-white mr-3" />
+                  </a>
+
+                  <a
+                    href="https://www.linkedin.com/in/instalando/"
+                    target="_blank"
+                  >
+                    <i class="fa-brands fa-linkedin text-2xl text-gray-400 hover:text-white mr-3" />
+                  </a>
+
+                  <a
+                    href="https://t.me/Instalando"
+                    target="_blank"
+                  >
+                    <i class="fa-brands fa-telegram text-2xl text-gray-400 hover:text-white" />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
