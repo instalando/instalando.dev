e0b97129423753cbd349197c15db9a8d523a4fa7
instalando <instalando@protonmail.com>
2022-04-18 12:20:57 -0300

    Changed Packer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,7 +26,7 @@ const skills = {
   ],
   devops: [
     'https://img.shields.io/badge/terraform-%235835CC.svg?style=for-the-badge&logo=terraform&logoColor=white',
-    'https://img.shields.io/badge/Packer-%235835CC.svg?style=for-the-badge&logo=terraform&logoColor=white'
+    'https://img.shields.io/badge/Packer-1DAEFF?style=for-the-badge&logo=packer&logoColor=white'
   ],
   tools: [
     'https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white',
