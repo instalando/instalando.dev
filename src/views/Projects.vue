5d1c0ed673c2fb9983cb644077d6e97952a20c4b
instalando <instalando@protonmail.com>
2022-04-18 08:38:51 -0300

    Fix 4k screen

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -26,7 +26,7 @@ const projects: ProjectItem[] = [
 </script>
 
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-2 cursor-pointer">
+  <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-2 cursor-pointer max-w-screen-xl">
     <div
       v-for="item in projects"
       :key="item.name"
